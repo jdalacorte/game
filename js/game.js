d2fa5f469e6cffb4d8ec511c416aeda801744d3b
jonas <jonas.dalacorte@gmail.com>
2020-08-18 01:52:51 -0300

    update

--- js/game.js
+++ js/game.js
@@ -12,17 +12,18 @@ setState: { *state*: true },
 
 */
 // names
-const alien = '<span class="var">Kroptilo</span>';
-const nomeDoBar = '<span class="var">Nome do Bar</span>';
+const alien = '<span class="var">Kroptiliano</span>';
+const nomeDoBar = `<span class="var">Mondrian's</span>`;
 const liquidoRoxo = '<span class="var">Líquido Roxo</span>';
 const codigoFrasco = '<span class="var">138-POx</span>';
 const draName = '<span class="var">Y.Suzuki</span>';
-const barman = 'Kleitonn';
+const barman = 'Nestór';
 const nomeDoCarro = '<span class="var">4-BIK-20</span>';
 const planetB = '<span class="var">Kluster-89</span>';
-const cigarroDeArtista = 'cigarro Kroptiliano';
-const plantaLouca = '<span class="var">Alternanthera Ficoidea</span>';
+const cigarroDeArtista = '<span class="var">cigarro Kroptiliano</span>';
+const plantaLouca = '<span class="var"><em>Alternanthera Ficoidea</em></span>';
 const substanciaB = '<span class="var">B7-DHJD</span>';
+const bonusScene = '';
 
 const textElement = document.getElementById('text');
 const optionButtonsElement = document.getElementById('option-buttons');
@@ -77,9 +78,9 @@ const textNodes = [
       <h1>
         SPACE MUAMBA 3020<sub class="line">demo</sub>
       </h1>
-      <p>
+      <h3>
         Medo e delírio no Limiar da Galáxia
-      </p>
+      </h3>
       <br>
       <p class="line">
         Criado por Daniel Hogrefe & Jonas Dalacorte
@@ -89,7 +90,7 @@ const textNodes = [
       '<img class="img" src="https://i.pinimg.com/originals/21/10/9a/21109a8e06845655f1d28fb8346add75.jpg" alt="test-img" />',
     options: [
       {
-        text: 'Começar',
+        text: '<span class="var">COMEÇAR</span>',
         nextText: 1,
       },
       {
@@ -105,13 +106,13 @@ const textNodes = [
     text: `<div class="title">
       <h1> Space Muamba 3020</h1></div>
   <div class="credits">
-    <h4>Criação</h4>
+    <h4 class="var">Criação</h4>
     <p>Daniel Hogrefe & Jonas Dalacorte</p>
-    <h4>Arte</h4>
+    <h4 class="var">Arte</h4>
     <p>Daniel Hogrefe</p>
-    <h4>Mecanismo & Programação</h4>
+    <h4 class="var">Mecanismo & Programação</h4>
     <p>Jonas Dalacorte</p>
-    <h4>Trilha Original</h4>
+    <h4 class="var">Trilha Original</h4>
     <p>AEON & CRUISE NOIR</p>
   </div >`,
     img: '',
@@ -126,16 +127,16 @@ const textNodes = [
   // cena 1
   {
     id: 1,
-    text: `Você acorda no chão de uma das cabines no banheiro do ${nomeDoBar}, um bar de fim de noite no Limiar da Galáxia. Os únicos resquícios da noite passada são o vômito nas suas calças, uma dor de cabeça infernal e um frasco cheio até a metade com um ${liquidoRoxo} que parece remédio para dor de barriga no seu bolso esquerdo. Apenas mais uma noite no ${nomeDoBar}.`,
+    text: `<p>Você acorda no chão de uma das cabines no banheiro do ${nomeDoBar}, um bar de fim de noite no Limiar da Galáxia. Os únicos resquícios da noite passada são o vômito nas suas calças, uma dor de cabeça infernal e um frasco cheio até a metade com um ${liquidoRoxo} que parece remédio para dor de barriga no seu bolso esquerdo. Apenas mais uma noite no ${nomeDoBar}.</p>`,
     img:
       '<img class="img" src="https://i.pinimg.com/originals/21/10/9a/21109a8e06845655f1d28fb8346add75.jpg" alt="test-img" />',
     options: [
       {
-        text: 'Sair do banheiro',
+        text: '<span class="var">Sair</span> do banheiro',
         nextText: 3,
       },
       {
-        text: 'Vasculhar as outras cabines',
+        text: 'Investigar as <span class="var">outras cabines</span>',
         nextText: 2,
       },
       {
@@ -153,7 +154,7 @@ const textNodes = [
       '<img class="img" src="https://i.pinimg.com/originals/21/10/9a/21109a8e06845655f1d28fb8346add75.jpg" alt="test-img" />',
     options: [
       {
-        text: 'Sair do banheiro',
+        text: '<span class="var">Sair</span> do banheiro',
         setState: { papelCodigo: true },
         nextText: 3,
       },
@@ -168,7 +169,7 @@ const textNodes = [
       '<img class="img" src="https://i.pinimg.com/originals/21/10/9a/21109a8e06845655f1d28fb8346add75.jpg" alt="test-img" />',
     options: [
       {
-        text: 'Perguntar sobre a noite passada',
+        text: 'Perguntar sobre a <span class="var">noite passada</span>',
         nextText: 6,
       },
       {
@@ -177,7 +178,7 @@ const textNodes = [
         requiredState: (currentState) => currentState.papelCodigo,
       },
       {
-        text: 'Dar de ombros e sair do bar',
+        text: 'Dar de ombros e <span class="var">sair do bar</span>',
         nextText: 7,
       },
     ],
@@ -191,7 +192,7 @@ const textNodes = [
       '<img class="img" src="https://i.pinimg.com/originals/21/10/9a/21109a8e06845655f1d28fb8346add75.jpg" alt="test-img" />',
     options: [
       {
-        text: 'Próximo',
+        text: '<span class="var">>>></span>',
         nextText: 5,
       },
     ],
@@ -205,11 +206,11 @@ const textNodes = [
       '<img class="img" src="https://i.pinimg.com/originals/21/10/9a/21109a8e06845655f1d28fb8346add75.jpg" alt="test-img" />',
     options: [
       {
-        text: '"Sem chance!", é hora de dar o fora',
+        text: '<span class="var">"Sem chance!"</span>, é hora de dar o fora',
         nextText: 7,
       },
       {
-        text: 'Deixar o Velho matar a curiosidade',
+        text: `Entregar A Substância ao Velho ${alien}`,
         nextText: 52,
       },
       {
@@ -226,7 +227,7 @@ const textNodes = [
       '<img class="img" src="https://i.pinimg.com/originals/21/10/9a/21109a8e06845655f1d28fb8346add75.jpg" alt="test-img" />',
     options: [
       {
-        text: 'Sair do bar',
+        text: '<span class="var">>>></span>',
         nextText: 7,
       },
     ],
@@ -248,16 +249,18 @@ const textNodes = [
       '<img class="img" src="https://i.pinimg.com/originals/21/10/9a/21109a8e06845655f1d28fb8346add75.jpg" alt="test-img" />',
     options: [
       {
-        text: `Procurar algo por perto para quebrar o vidro do ${nomeDoCarro}`,
+        text: `Procurar algo para quebrar o vidro do ${nomeDoCarro}`,
         setState: { carro: true },
         nextText: 8,
       },
       {
-        text: '“Que diabos de dia”, experimentar A Substância',
+        text:
+          '<span class="var">"Que diabos de dia"</span>, experimentar A Substância',
         nextText: 51,
       },
       {
-        text: 'Chamar um Táxi Espacial e curar a ressaca em casa',
+        text:
+          'Chamar um <span class="var">Táxi Espacial</span> e curar a ressaca em casa',
         setState: { taxi: true },
         nextText: 10,
       },
@@ -273,12 +276,13 @@ const textNodes = [
       '<img class="img" src="https://i.pinimg.com/originals/21/10/9a/21109a8e06845655f1d28fb8346add75.jpg" alt="test-img" />',
     options: [
       {
-        text: 'Ouvir a mensagem',
+        text: '<span class="var">Abrir</span> a mensagem',
         setState: { mensagemOK: true },
         nextText: 9,
       },
       {
-        text: 'Ignorar a mensagem e ir pra casa curar a ressaca',
+        text:
+          '<span class="var">Ignorar</span> a mensagem e ir pra casa curar a ressaca',
         setState: { taxi: true },
         nextText: 10,
       },
@@ -294,11 +298,10 @@ const textNodes = [
       '<img class="img" src="https://i.pinimg.com/originals/21/10/9a/21109a8e06845655f1d28fb8346add75.jpg" alt="test-img" />',
     options: [
       {
-        text: 'Ignorar as instruções e ir para casa curar a ressaca',
+        text:
+          '<span class="var">Ignorar</span> as instruções e ir para casa curar a ressaca',
         nextText: 10,
-        requiredState: (currentState) =>
-          currentState.carro || !currentState.taxi,
-        setState: { mensagemOK: true },
+        requiredState: (currentState) => currentState.mensagemOK,
       },
       {
         text: `Seguir as instruções e saltar para ${planetB}`,
@@ -306,8 +309,6 @@ const textNodes = [
       },
       {
         text: `Jogar o código e o nome da Dra. ${draName} na Rede de Computadores`,
-        requiredState: (currentState) => !currentState.google,
-        setState: { mensagemOK: true, google: true },
         nextText: 11,
       },
     ],
@@ -327,13 +328,13 @@ const textNodes = [
         nextText: 12,
       },
       {
-        text: 'Experimentar a maldita Substância',
+        text: 'Experimentar a maldita <span class="var">Substância</span>',
         nextText: 51,
       },
       {
-        text: `Abrir sua caixa de mensagens`,
+        text: `Checar sua <span class="var">caixa de mensagens</span>`,
         requiredState: (currentState) => currentState.taxi,
-        setState: { mensagemOK: true },
+        setState: { mensagemOK: false },
         nextText: 9,
       },
     ],
@@ -347,20 +348,8 @@ const textNodes = [
       '<img class="img" src="https://i.pinimg.com/originals/21/10/9a/21109a8e06845655f1d28fb8346add75.jpg" alt="test-img" />',
     options: [
       {
-        text: `Seguir as Instruções e saltar para ${planetB}`,
-        requiredState: (currentState) => currentState.mensagemOK,
+        text: `Saltar para ${planetB}`,
         nextText: 12,
-      },
-      {
-        text: 'Ignorar as instruções e ir para casa curar a ressaca',
-        requiredState: (currentState) =>
-          currentState.mensagemOK && currentState.carro,
-        nextText: 10,
-      },
-      {
-        text: `Abrir sua caixa de mensagens`,
-        requiredState: (currentState) => currentState.taxi,
-        nextText: 9,
       },
     ],
   },
@@ -379,7 +368,7 @@ const textNodes = [
       '<img class="img" src="https://i.pinimg.com/originals/21/10/9a/21109a8e06845655f1d28fb8346add75.jpg" alt="test-img" />',
     options: [
       {
-        text: 'Próximo',
+        text: '<span class="var">>>></span>',
         nextText: 13,
       },
     ],
@@ -392,11 +381,11 @@ const textNodes = [
       '<img class="img" src="https://i.pinimg.com/originals/21/10/9a/21109a8e06845655f1d28fb8346add75.jpg" alt="test-img" />',
     options: [
       {
-        text: 'Investigar o Bar',
+        text: 'Investigar o <span class="var">Bar</span>',
         nextText: 15,
       },
       {
-        text: 'Investigar a Névoa',
+        text: 'Investigar a <span class="var">Névoa</span>',
         setState: { barB: true },
         nextText: 16,
       },
@@ -415,11 +404,11 @@ const textNodes = [
       '<img class="img" src="https://i.pinimg.com/originals/21/10/9a/21109a8e06845655f1d28fb8346add75.jpg" alt="test-img" />',
     options: [
       {
-        text: 'Investigar o Bar',
+        text: 'Investigar o <span class="var">Bar</span>',
         nextText: 15,
       },
       {
-        text: 'Investigar a Névoa',
+        text: 'Investigar a <span class="var">Névoa</span>',
         setState: { barB: true },
         nextText: 16,
       },
@@ -433,15 +422,16 @@ const textNodes = [
       '<img class="img" src="https://i.pinimg.com/originals/21/10/9a/21109a8e06845655f1d28fb8346add75.jpg" alt="test-img" />',
     options: [
       {
-        text: 'Perguntar ao Barman sobre a Névoa Roxa',
+        text: 'Perguntar ao Barman sobre a <span class="var">Névoa Roxa</span>',
         nextText: 17,
       },
       {
-        text: 'Pedir uma dose da Bebida ao Barman',
+        text: 'Pedir uma dose da <span class="var">Bebida</span>',
         nextText: 51,
       },
       {
-        text: '"Dane-se esse lugar", seguir em direção à Névoa',
+        text:
+          '<span class="var">"Dane-se esse lugar"</span>, seguir em direção à Névoa',
         setState: { barB: true },
         nextText: 16,
       },
@@ -456,16 +446,19 @@ const textNodes = [
       '<img class="img" src="https://i.pinimg.com/originals/21/10/9a/21109a8e06845655f1d28fb8346add75.jpg" alt="test-img" />',
     options: [
       {
-        text: 'Esconder-se e observar a Figura Enigmática secretamente',
+        text:
+          'Esconder-se e <span class="var">observar</span> a Figura Enigmática secretamente',
         nextText: 18,
       },
       {
-        text: 'Voltar ao Bar e tentar conseguir mais informações sobre o local',
+        text:
+          '<span class="var">Voltar ao Bar</span> e tentar conseguir mais informações sobre o local',
         requiredState: (currentState) => currentState.barB,
         nextText: 17,
       },
       {
-        text: 'Atacar a Figura Enigmática aproveitando o fator surpresa',
+        text:
+          '<span class="var">Atacar</span> a Figura Enigmática aproveitando o fator surpresa',
         nextText: 19,
       },
     ],
@@ -478,11 +471,11 @@ const textNodes = [
       '<img class="img" src="https://i.pinimg.com/originals/21/10/9a/21109a8e06845655f1d28fb8346add75.jpg" alt="test-img" />',
     options: [
       {
-        text: 'Pedir uma dose da Bebida ao Barman',
+        text: 'Pedir uma dose da <span class="var">Bebida</span>',
         nextText: 51,
       },
       {
-        text: 'Investigar a Névoa',
+        text: 'Investigar a <span class="var">Névoa</span>',
         setState: { barB: false },
         nextText: 16,
       },
@@ -496,7 +489,7 @@ const textNodes = [
       '<img class="img" src="https://i.pinimg.com/originals/21/10/9a/21109a8e06845655f1d28fb8346add75.jpg" alt="test-img" />',
     options: [
       {
-        text: 'Próximo',
+        text: '<span class="var">>>></span>',
         nextText: 20,
       },
     ],
@@ -509,7 +502,7 @@ const textNodes = [
       '<img class="img" src="https://i.pinimg.com/originals/21/10/9a/21109a8e06845655f1d28fb8346add75.jpg" alt="test-img" />',
     options: [
       {
-        text: 'Próximo',
+        text: '<span class="var">>>></span>',
         nextText: 20,
       },
     ],
@@ -522,7 +515,7 @@ const textNodes = [
       '<img class="img" src="https://i.pinimg.com/originals/21/10/9a/21109a8e06845655f1d28fb8346add75.jpg" alt="test-img" />',
     options: [
       {
-        text: 'Aceitar a dose',
+        text: '<span class="var">Aceitar</span> a dose',
         nextText: 53,
       },
     ],
@@ -535,7 +528,7 @@ const textNodes = [
       '<img class="img" src="https://i.pinimg.com/originals/21/10/9a/21109a8e06845655f1d28fb8346add75.jpg" alt="test-img" />',
     options: [
       {
-        text: 'FIM',
+        text: '<span class="var">FIM</span>',
         nextText: 69,
       },
     ],
@@ -548,7 +541,7 @@ const textNodes = [
       '<img class="img" src="https://i.pinimg.com/originals/21/10/9a/21109a8e06845655f1d28fb8346add75.jpg" alt="test-img" />',
     options: [
       {
-        text: 'FIM',
+        text: '<span class="var">FIM</span>',
         nextText: 69,
       },
     ],
@@ -556,7 +549,7 @@ const textNodes = [
   // fim 3
   {
     id: 53,
-    text: ` descrever alguma loucura estilo medo e delirio em las vegas (vou ler de novo pra inspirar) ou burroughs kkkkk`,
+    text: `Final no. 3`,
     img:
       '<img class="img" src="https://i.pinimg.com/originals/21/10/9a/21109a8e06845655f1d28fb8346add75.jpg" alt="test-img" />',
     options: [
@@ -569,7 +562,7 @@ const textNodes = [
   // BONUS
   {
     id: 69,
-    text: `nessa tela pode aparecer o bagulho do download do disco/ep etc essas coisa coisetal`,
+    text: `${bonusScene}`,
     img:
       '<img class="img" src="https://i.pinimg.com/originals/21/10/9a/21109a8e06845655f1d28fb8346add75.jpg" alt="test-img" />',
     options: [
@@ -585,7 +578,7 @@ startGame();
 
 $(document).ready(function () {
   $('i').click(function () {
-    $('i').toggleClass('fas fa-play-circle fa-2x fas fa-pause-circle fa-2x');
+    $('i').toggleClass('fas fa-play fa-2x fas fa-pause fa-2x');
   });
 });
 
