d35711ff4d18ab980a5c71021e129b887d3e7652
jonas <jonas.dalacorte@gmail.com>
2020-09-05 12:40:40 -0300

    update

--- js/game.js
+++ js/game.js
@@ -1,4 +1,4 @@
-const gameName = 'L4-Q20';
+const gameName = 'G4-NJ4';
 const bonusScene = ``;
 
 const imageElement = document.getElementById('img');
@@ -50,13 +50,13 @@ const textNodes = [
     id: 0,
     text:
       `<div class="title">
-      <h1 class="main-title anim-txt">${gameName}<sub class="subline">demo</sub></h1>
+      <h1 class="main-title anim-txt">${gameName}</h1>
       <h3 class="subtitle">Medo e delírio no Limiar da Galáxia</h3>
       <br>
       <p class="line animate__animated animate__slideInUp animate__faster">Criado por Daniel Hogrefe & Jonas Dalacorte</p>
     </div>`,
     img:
-      '<img class="img" src="https://64.media.tumblr.com/23ff7ca23c6bae22fe8d430cf5dbef3d/102dfc3987410b01-94/s1280x1920/959f41fd90cf6acc8ba9d226b432640091803b3b.jpg" alt="copyright Daniel Hogrefe" />',
+      '<img class="img" src="../img/bg.jpeg" alt="copyright Daniel Hogrefe" />',
     options: [
       {
         text: '<strong>Jogar ✨</strong>',
@@ -74,7 +74,7 @@ const textNodes = [
     id: 24,
     text:
       `<div class="title">
-      <h1 class="credits-title animate__animated animate__lightSpeedInRight animate__fast">${gameName}<sub class="subline">demo</sub></h1>
+      <h1 class="credits-title animate__animated animate__lightSpeedInRight animate__fast">${gameName}</h1>
     </div>
   <div class="credits">
     <h4 class="credits-title">Criação</h4>
@@ -119,7 +119,7 @@ const textNodes = [
   // SCENE 2
   {
     id: 2,
-    text: `<p class="txt animate__animated animate__fadeIn">Além de um maço de cigarros úmidos e um rato espacial em decomposição, nada de útil na cabine da direita. Na cabine à sua esquerda você encontra uma prescrição médica rasgada pela metade. É possível ler apenas "<span class="var">Dra. Y.Suzuki</span>" e o código "<span class="var">L4-Q20</span>" &mdash; essa mesma sequência está anotada no frasco do Líquido Roxo.<span class="cursor-blink">|</span></p>`,
+    text: `<p class="txt animate__animated animate__fadeIn">Além de um maço de cigarros úmidos e um rato espacial em decomposição, nada de útil na cabine da direita. Na cabine à sua esquerda você encontra uma prescrição médica rasgada pela metade. É possível ler apenas "<span class="var">Dra. Y.Suzuki</span>" e o código "<span class="var">G4-NJ4</span>" &mdash; essa mesma sequência está anotada no frasco do Líquido Roxo.<span class="cursor-blink">|</span></p>`,
     img: '<img class="img" src="./img/ph.jpg" alt="copyright Daniel Hogrefe" />',
     options: [
       {
@@ -145,7 +145,7 @@ const textNodes = [
         nextText: 6,
       },
       {
-        text: 'Perguntar o que ele sabe sobre A Substância <strong>L4-Q20</strong>',
+        text: 'Perguntar o que ele sabe sobre A Substância <strong>G4-NJ4</strong>',
         nextText: 4,
         requiredState: (currentState) => currentState.clue1,
       },
@@ -172,7 +172,7 @@ const textNodes = [
   // SCENE 5
   {
     id: 5,
-    text: `<p class="txt animate__animated animate__fadeIn">O Velho Barman explica que A Substância <span class="var">L4-Q20</span> não é produzida há mais de 300 anos, antes dos rumores sobre A Guerra Iminente, e que provavelmente esse frasco veio parar nesse exato buraco no Limiar da Galáxia por meio de Contrabando Sônico &mdash; uma espécie de Viagem no Tempo. Suas propriedades são desconhecidas já que sua principal pesquisadora, a <span class="var">Dra. Y.Suzuki</span>, desapareceu logo após a descoberta. Ele muda de feição repentinamente e pede para ver A Substância mais de perto, as luzes do bar começam a piscar bizarramente.<span class="cursor-blink">|</strong></p>`,
+    text: `<p class="txt animate__animated animate__fadeIn">O Velho Barman explica que A Substância <span class="var">G4-NJ4</span> não é produzida há mais de 300 anos, antes dos rumores sobre A Guerra Iminente, e que provavelmente esse frasco veio parar nesse exato buraco no Limiar da Galáxia por meio de Contrabando Sônico &mdash; uma espécie de Viagem no Tempo. Suas propriedades são desconhecidas já que sua principal pesquisadora, a <span class="var">Dra. Y.Suzuki</span>, desapareceu logo após a descoberta. Ele muda de feição repentinamente e pede para ver A Substância mais de perto, as luzes do bar começam a piscar bizarramente.<span class="cursor-blink">|</strong></p>`,
     img: '<img class="img" src="./img/ph.jpg" alt="copyright Daniel Hogrefe" />',
     options: [
       {
@@ -451,7 +451,7 @@ const textNodes = [
   // SCENE 19
   {
     id: 19,
-    text: `<p class="txt animate__animated animate__fadeIn">A Dr. Y.Suzuki chega até você num caminhar flutuante. Suas pupilas estão totalmente dilatadas, os olhos são como duas azeitonas inchadas: "É tudo uma simples simulação. Nós conseguimos quebrar o Universo. Descobrimos o caminho pra Fora. Eu tenho percorrido o Tempo tentando encontrar um jeito de reunir minhas descobertas. O <span class="var">L4-Q20</span> que você trouxe até mim é um extrato de <span class="var"><em>Alternanthera Ficoidea</em></span>. Ele dissolve as amarras do programa. Libertando a mente. Mas é fatal. Eles não querem que isso seja do conhecimento de ninguém que julguem perigosos ou revolucionários aos olhos do Pseudo-Império. Desde os primeiros rumores sobre a Guerra Iminente, há 300 anos, todas as informações sobre As Substâncias têm sido apagadas da história.”. A Doutora mostra o frasco que ela tem em mãos e você percebe que a Substância tem uma cor levemente diferente. "Essa é minha outra descoberta: <span class="var">A Substância M1-C03</span> age no organismo limitando o efeito da Explosão Sônica causada pela L4-Q20. As duas devem ser usadas juntas em doses exatamente iguais." Ela mistura as duas Substâncias e oferece uma dose a você.<span class="cursor-blink">|</strong></p>`,
+    text: `<p class="txt animate__animated animate__fadeIn">A Dr. Y.Suzuki chega até você num caminhar flutuante. Suas pupilas estão totalmente dilatadas, os olhos são como duas azeitonas inchadas: "É tudo uma simples simulação. Nós conseguimos quebrar o Universo. Descobrimos o caminho pra Fora. Eu tenho percorrido o Tempo tentando encontrar um jeito de reunir minhas descobertas. O <span class="var">G4-NJ4</span> que você trouxe até mim é um extrato de <span class="var"><em>Alternanthera Ficoidea</em></span>. Ele dissolve as amarras do programa. Libertando a mente. Mas é fatal. Eles não querem que isso seja do conhecimento de ninguém que julguem perigosos ou revolucionários aos olhos do Pseudo-Império. Desde os primeiros rumores sobre a Guerra Iminente, há 300 anos, todas as informações sobre As Substâncias têm sido apagadas da história.”. A Doutora mostra o frasco que ela tem em mãos e você percebe que a Substância tem uma cor levemente diferente. "Essa é minha outra descoberta: <span class="var">A Substância H5-MK5</span> age no organismo limitando o efeito da Explosão Sônica causada pela G4-NJ4. As duas devem ser usadas juntas em doses exatamente iguais." Ela mistura as duas Substâncias e oferece uma dose a você.<span class="cursor-blink">|</strong></p>`,
     img: '<img class="img" src="./img/ph.jpg" alt="copyright Daniel Hogrefe" />',
     options: [
       {
