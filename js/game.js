ec3557845109c815c6e4d3e89ea37088117b4cfb
jonas <jonas.dalacorte@gmail.com>
2020-09-06 19:05:24 -0300

    update

--- js/game.js
+++ js/game.js
@@ -16,7 +16,7 @@ function showTextNode(textNodeIndex) {
   textElement.innerHTML = textNode.text;
   if (textNodeIndex === 22) {
     textElement.style.visibility = "hidden";
-    document.body.style.backgroundImage = "url('./img/bg.jpg')";
+    document.body.style.backgroundImage = "url('./img/bg-2.jpg')";
   } else {
     textElement.style.visibility = "visible";
   }
@@ -65,7 +65,7 @@ const textNodes = [
       <p class="line animate__animated animate__slideInUp animate__faster">Criado por Daniel Hogrefe & Jonas Dalacorte</p>
     </div>`,
     img:
-      '<img class="img" src="./img/bg.jpeg" alt="copyright Daniel Hogrefe" />',
+      '<img class="img" src="./img/ph.jpg" alt="copyright Daniel Hogrefe" />',
     options: [
       {
         text: '<strong>Jogar ✨</strong>',
@@ -112,7 +112,7 @@ const textNodes = [
   {
     id: 1,
     text: `<p class="txt animate__animated animate__fadeIn">Você acorda no chão imundo de uma das cabines no banheiro do <span class="var">Mondrian's</span>, um bar de fim de noite no Limiar da Galáxia. Os únicos resquícios da noite passada são o vômito nas suas calças, uma dor de cabeça infernal e um frasco cheio até a metade com um <span class="var">Líquido Roxo</span> que parece remédio para dor de barriga no seu bolso esquerdo.<span class="cursor-blink">|</span></p>`,
-    img: '<img class="img" src="./img/bg.jpeg" alt="copyright Daniel Hogrefe" />',
+    img: '<img class="img" src="./img/ph.jpg" alt="copyright Daniel Hogrefe" />',
     options: [
       {
         text: '<strong>Sair</strong> do banheiro',
